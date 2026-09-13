a047afbe4b96a2de610cf85135be22202354edeb
Hassan bin Arsalan <hassanbinarsalan@gmail.com>
2026-09-13 22:30:47 +0500

    Add files via upload

---
+++ Escape Sequences.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("Student Information:\n");
+    printf("\tName: \"Your Name\"\n");
+    printf("\tRoll Number: 123\n");
+    printf("\tSection: A\n\n");
+
+    printf("Path: C:\\Users\\Student");
+
+    return 0;
+}
